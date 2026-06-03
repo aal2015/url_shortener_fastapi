9001188b127b2afc9671a42b8f9b4904ab13b222
aal2015 <abhinavlugun@yahoo.com>
2026-06-03 12:49:53 +0530

    Added tests for create short codes and get original url

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from main import app, generate_short_code
 from database import Base, engine, get_db
 from unittest.mock import patch
+import pytest
 
 DATABASE_URL = "sqlite:///:memory:"
 
@@ -30,10 +31,23 @@ def override_get_db():
 
 app.dependency_overrides[get_db] = override_get_db
 
+@pytest.fixture(scope="module", autouse=True)
+def setup_database():
+
+    Base.metadata.create_all(bind=engine)
+
+    yield
+
+    Base.metadata.drop_all(bind=engine)
+
+######### Base root test
+
 def test_read_root():
     response = client.get("")
     assert response.status_code == 200
     assert response.json() == "Server is running"
+
+######### generate_short_code function test
 
 def test_generate_short_code_default_length(length=6):
     code = generate_short_code()
@@ -74,8 +88,90 @@ def test_generate_short_code_mocked():
 
     assert code == "AAAA"
 
-def setup():
-    Base.metadata.create_all(bind=engine)
+######### short code creation test
 
-def teardown():
-    Base.metadata.drop_all(bind=engine)
+def test_create_short_url():
+
+    response = client.post(
+        "/generateShortURL",
+        json={
+            "long_url": "https://google.com"
+        }
+    )
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data["message"] == "Short URL created"
+    assert len(data["short_code"]) == 6
+
+def test_custom_alias():
+
+    response = client.post(
+        "/generateShortURL",
+        json={
+            "long_url": "https://openai.com",
+            "custom_alias": "openai"
+        }
+    )
+
+    assert response.status_code == 200
+
+    assert response.json()["short_code"] == "openai"
+
+def test_duplicate_alias():
+
+    client.post(
+        "/generateShortURL",
+        json={
+            "long_url": "https://site1.com",
+            "custom_alias": "alias123"
+        }
+    )
+
+    response = client.post(
+        "/generateShortURL",
+        json={
+            "long_url": "https://site2.com",
+            "custom_alias": "alias123"
+        }
+    )
+
+    assert response.status_code == 400
+
+    assert response.json() == {
+        "detail": "Custom alias already exists"
+    }
+
+def test_get_original_url():
+
+    client.post(
+        "/generateShortURL",
+        json={
+            "long_url": "https://stackoverflow.com",
+            "custom_alias": "stack"
+        }
+    )
+
+    response = client.get(
+        "/getOriginalURL/stack"
+    )
+
+    assert response.status_code == 200
+
+    assert response.json() == {
+        "long_url": "https://stackoverflow.com"
+    }
+
+def test_url_not_found():
+
+    response = client.get(
+        "/getOriginalURL/notfound"
+    )
+
+    assert response.status_code == 404
+
+    assert response.json() == {
+        "detail": "Short URL not found"
+    }
